933df9c1175a07f51fab395b21c0f3d16d58e6a1
hoondeveloper <hoonskyn@naver.com>
2021-02-16 02:03:24 +0900

    feat: show json data of github api access_token from server

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,39 @@
 import { Route, Switch, useHistory } from 'react-router-dom';
 import SignIn, { ISignInProps } from '../oauth/SignIn';
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { toParams, toQuery } from '../utils/url';
+import axios from 'axios';
+import ReactJson from 'react-json-view';
 
 const SignInData: Partial<ISignInProps> = {
   clientId: '0b05ada88d3a8667fccf',
   redirectUri: 'http://localhost:3000/oauth_redirect',
 };
 
+const CLIENT_SECRET = '0d5ed981f1e024e5a0e6e0e5e9ffa73d1fb1afc7';
+
 const Main: React.FC = () => {
   const history = useHistory();
+  const [accessToken, setAccessToken] = useState({});
+  const getGitHubAccessToken = useCallback(async () => {
+    const res = await axios.post('http://3.34.194.121:8000/oauth/', body);
+    return res.data;
+  }, []);
+  const body = useMemo(
+    () => ({
+      ...toParams(history.location.search),
+      client_id: SignInData.clientId,
+      client_secret: CLIENT_SECRET,
+    }),
+    [history.location.search],
+  );
+
+  useEffect(() => {
+    getGitHubAccessToken().then(res => {
+      setAccessToken(res);
+    });
+  }, []);
+
   return (
     <>
       <Switch>
@@ -29,7 +54,21 @@ const Main: React.FC = () => {
           />
         </Route>
         <Route path="/oauth_redirect">
-          <span>Hi</span>
+          <div>
+            <h1>아래의 코드를 Body에 입력해서 사용하세요.</h1>
+            <h3>GitHub API Get Access Token Response</h3>
+            <div
+              style={{ textAlign: 'left', maxWidth: '800px', margin: '0 auto' }}
+            >
+              <ReactJson src={accessToken} />
+            </div>
+            <h3>요청한 Body</h3>
+            <div
+              style={{ textAlign: 'left', maxWidth: '800px', margin: '0 auto' }}
+            >
+              <ReactJson src={body} />
+            </div>
+          </div>
         </Route>
       </Switch>
     </>
