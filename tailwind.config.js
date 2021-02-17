8b90e87f945d30022c1edc0ddd5a6da91975a838
hoondeveloper <hoonskyn@naver.com>
2021-02-18 02:13:14 +0900

    feat: add tailwind color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      gray: {
+        50: '#F9F9F9',
+        100: '#F2F2F2',
+        200: '#E3E3E3',
+        300: '#D4D4D4',
+        400: '#C4C4C4',
+        500: '#B5B5B5',
+        600: '#969696',
+        700: '#6B6B6B',
+        800: '#3D3D3D',
+        900: '#222222',
+        default: '#B5B5B5',
+      },
+    },
   },
   variants: {
     extend: {},
