c33730ce46265c0978103637e44009d076af2890
hoondeveloper <hoonskyn@naver.com>
2021-02-18 02:16:38 +0900

    feat: Routing Logic entirely

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,52 +1,41 @@
-import { Route, Switch, useHistory } from 'react-router-dom';
-import SignIn, { ISignInProps } from '../oauth/SignIn';
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { toParams, toQuery } from '../utils/url';
-import axios from 'axios';
-import ReactJson from 'react-json-view';
-
-const SignInData: Partial<ISignInProps> = {
-  clientId: '0b05ada88d3a8667fccf',
-  redirectUri: 'https://frontend-jandi-s-value.vercel.app/oauth_redirect',
-};
-
-const CLIENT_SECRET = '0d5ed981f1e024e5a0e6e0e5e9ffa73d1fb1afc7';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import SignIn from '../oauth/SignIn';
+import React from 'react';
+import Intro from './intro/Intro';
+import GitHubAuthRedirect from 'oauth/GitHubAuthRedirect';
+import { SignInData } from 'oauth/AuthData';
+import ConditionalRoute from 'components/layout/ConditionalRoute';
+import { useLocalStorage } from '@rehooks/local-storage';
+import { message } from 'antd';
 
 const Main: React.FC = () => {
-  const history = useHistory();
-  const [accessToken, setAccessToken] = useState({});
-  const getGitHubAccessToken = async (body: { [key: string]: any }) => {
-    const res = await axios.post('https://3.34.194.121:8000/oauth/', body);
-    return res.data;
-  };
-  const body = useMemo(
-    () => ({
-      ...toParams(history.location.search),
-      client_id: SignInData.clientId,
-      client_secret: CLIENT_SECRET,
-    }),
-    [history.location.search],
-  );
-
-  useEffect(() => {
-    getGitHubAccessToken(body).then(res => {
-      setAccessToken(res);
-    });
-  }, [body]);
+  const [accessToken] = useLocalStorage<string>('access_token', '');
 
   return (
     <>
       <Switch>
-        <Route exact path="/">
-          <h1>Jandi GitHub OAuth Test</h1>
-          <button
-            onClick={() => {
-              history.replace('/signin');
-            }}
-          >
-            SignIn
-          </button>
-        </Route>
+        <Route
+          exact
+          path="/"
+          render={({ location }) => (
+            <Redirect
+              to={{
+                pathname: '/app',
+                state: { from: location },
+              }}
+            />
+          )}
+        />
+        <ConditionalRoute
+          path="/intro"
+          redirectPath="/app"
+          condition={!(!!accessToken && accessToken !== '')}
+          onFalse={() => {
+            message.error('이미 로그인 정보가 있습니다.');
+          }}
+        >
+          <Intro />
+        </ConditionalRoute>
         <Route path="/signin">
           <SignIn
             clientId={SignInData.clientId}
@@ -54,22 +43,18 @@ const Main: React.FC = () => {
           />
         </Route>
         <Route path="/oauth_redirect">
-          <div>
-            <h1>아래의 코드를 Body에 입력해서 사용하세요.</h1>
-            <h3>GitHub API Get Access Token Response</h3>
-            <div
-              style={{ textAlign: 'left', maxWidth: '800px', margin: '0 auto' }}
-            >
-              <ReactJson src={accessToken} />
-            </div>
-            <h3>요청한 Body</h3>
-            <div
-              style={{ textAlign: 'left', maxWidth: '800px', margin: '0 auto' }}
-            >
-              <ReactJson src={body} />
-            </div>
-          </div>
+          <GitHubAuthRedirect />
         </Route>
+        <ConditionalRoute
+          path="/app"
+          redirectPath="/intro"
+          condition={!!accessToken && accessToken !== ''}
+          onTrue={() => {
+            message.info('로그인이 정상 처리되었습니다.');
+          }}
+        >
+          <div>TEST</div>
+        </ConditionalRoute>
       </Switch>
     </>
   );
