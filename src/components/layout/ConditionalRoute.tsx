36fdf63c63a02f788671ec13ca4e87fe63d2e73c
hoondeveloper <hoonskyn@naver.com>
2021-02-18 02:14:30 +0900

    feat: ConditionalRoute component

---
+++ src/components/layout/ConditionalRoute.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { RouteProps } from 'react-router';
+import { message } from 'antd';
+import { useLocalStorage } from '@rehooks/local-storage';
+
+const ConditionalRoute: React.FC<
+  RouteProps & {
+    condition: boolean;
+    onTrue?: () => any;
+    onFalse?: () => any;
+    redirectPath: string;
+  }
+> = ({
+  children,
+  condition,
+  onTrue,
+  onFalse,
+  redirectPath,
+  ...remainingProps
+}) => {
+  useEffect(() => {
+    if (condition) {
+      onTrue && onTrue();
+    } else {
+      onFalse && onFalse();
+    }
+  }, []);
+  return (
+    <Route
+      {...remainingProps}
+      render={({ location }) => {
+        if (condition) {
+          return children;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: redirectPath,
+                state: { from: location },
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
+export default ConditionalRoute;
