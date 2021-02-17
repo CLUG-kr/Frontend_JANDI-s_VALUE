c983464aeed94faa738c5951bfdfa7eddb2209e1
hoondeveloper <hoonskyn@naver.com>
2021-02-18 02:15:00 +0900

    feat: Footer component

---
+++ src/components/layout/footer/Footer.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import JandevelopLogo from 'assets/images/jandevelop-logo.svg';
+import tw from 'twin.macro';
+
+const FooterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  padding: 6rem 0 4rem;
+`;
+
+const LogoImage = styled.img`
+  height: 1.5rem;
+`;
+
+const Footer: React.FC = () => {
+  return (
+    <FooterWrapper>
+      <LogoImage src={JandevelopLogo} alt="Jandevelop Logo" />
+      <span css={tw`text-gray-500 mt-1 text-xs`}>
+        Copyright(c) 2021. JANDevelopers. All rights reserved.
+      </span>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
