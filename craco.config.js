85ef5a8c55a116b3da60d1f2a77bd6f43a670011
hoondeveloper <hoonskyn@naver.com>
2021-02-17 22:20:22 +0900

    feat: apply craco-alias

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CracoAlias = require('craco-alias');
+
 module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        tsConfigPath: 'tsconfig.path.json',
+      },
+    },
+  ],
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
