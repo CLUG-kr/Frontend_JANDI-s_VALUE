679d424fd6b6cc8b604997d407c95797de2a738b
hoondeveloper <hoonskyn@naver.com>
2021-02-21 01:56:02 +0900

    fix: replace tailwind color to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,21 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      gray: {
-        50: '#F9F9F9',
-        100: '#F2F2F2',
-        200: '#E3E3E3',
-        300: '#D4D4D4',
-        400: '#C4C4C4',
-        500: '#B5B5B5',
-        600: '#969696',
-        700: '#6B6B6B',
-        800: '#3D3D3D',
-        900: '#222222',
-        default: '#B5B5B5',
+    extend: {
+      colors: {
+        gray: {
+          50: '#F9F9F9',
+          100: '#F2F2F2',
+          200: '#E3E3E3',
+          300: '#D4D4D4',
+          400: '#C4C4C4',
+          500: '#B5B5B5',
+          600: '#969696',
+          700: '#6B6B6B',
+          800: '#3D3D3D',
+          900: '#222222',
+          default: '#B5B5B5',
+        },
       },
     },
   },
