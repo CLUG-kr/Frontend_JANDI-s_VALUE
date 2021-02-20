2f5533fc07b89e1f07f502ab91605033f7718e7a
hoondeveloper <hoonskyn@naver.com>
2021-02-21 04:33:07 +0900

    feat: Header Profile

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -2,6 +2,9 @@ import styled from '@emotion/styled';
 import React from 'react';
 import Logo from 'assets/images/logo.svg';
 import tw from 'twin.macro';
+import { IProfile } from '../../../models/DashboardModel';
+import { Avatar, Button } from 'antd';
+import Title from 'antd/es/typography/Title';
 
 const HeaderWrapper = styled.div`
   display: flex;
@@ -13,11 +16,44 @@ const HeaderLogo = styled.img`
   width: 45px;
 `;
 
-const Header: React.FC = () => {
+const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  transition: background 0.4s;
+
+  > * + * {
+    margin-left: 0.7rem;
+  }
+
+  ${tw`px-3 py-2 rounded-lg hover:bg-gray-100 cursor-pointer`}
+`;
+
+const ProfileDescription = styled.div`
+  text-align: left;
+`;
+
+interface IHeaderProps {
+  profile: IProfile | null;
+}
+
+const Header: React.FC<IHeaderProps> = ({ profile }) => {
   return (
     <>
       <HeaderWrapper>
         <HeaderLogo src={Logo} alt="logo" />
+        <a href={`https://github.com/${profile?.username}`} target="_blank">
+          <Profile>
+            <Avatar
+              size={24}
+              icon={<img src={profile?.profile_img_url} alt="profile image" />}
+            />
+            <ProfileDescription>
+              <Title level={5} css={tw`mb-0! text-gray-800!`}>
+                {profile?.username || '불러오는 중'}
+              </Title>
+            </ProfileDescription>
+          </Profile>
+        </a>
       </HeaderWrapper>
     </>
   );
