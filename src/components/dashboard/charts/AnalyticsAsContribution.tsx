8330a05e4c432d2b280d67601d228ec43ae6babc
hoondeveloper <hoonskyn@naver.com>
2021-02-22 01:46:09 +0900

    feat: change contribution analytics chart to bar

--- src/components/dashboard/charts/AnalyticsAsContribution.tsx
+++ src/components/dashboard/charts/AnalyticsAsContribution.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo } from 'react';
-import { WordCloud } from '@ant-design/charts';
+import { Bar } from '@ant-design/charts';
 import { IAnalyticsAsContribution } from '../../../models/DashboardModel';
-import { WordCloudConfig } from '@ant-design/charts/es/wordCloud';
+import { BarConfig } from '@ant-design/charts/es/bar';
 
 interface IAnalyticsAsContributionProps {
   data: IAnalyticsAsContribution[];
@@ -10,27 +10,18 @@ interface IAnalyticsAsContributionProps {
 const AnalyticsAsContribution: React.FC<IAnalyticsAsContributionProps> = ({
   data,
 }) => {
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
-  const config = useMemo<WordCloudConfig>(
+  const config = useMemo<BarConfig>(
     () => ({
       data: data,
       width: 370,
-      wordField: 'username',
-      weightField: 'value',
-      colorField: 'username',
-      wordStyle: {
-        fontFamily: 'Inter',
-        fontSize: [15, 40],
-        rotation: 0,
-      },
-      interactions: [{ type: 'element-active' }],
-      state: { active: { style: { lineWidth: 0 } } },
+      xField: 'value',
+      yField: 'username',
+      seriesField: 'username',
+      legend: { position: 'top-left' },
     }),
     [data],
   );
-  return <WordCloud {...config} />;
+  return <Bar {...config} />;
 };
 
 export default AnalyticsAsContribution;
