913b6de8540debf66e36af5a72317f22edb4c882
hoondeveloper <hoonskyn@naver.com>
2021-02-21 17:41:31 +0900

    refactor: detach dashboard model

--- src/models/DashboardModel.ts
+++ src/models/DashboardModel.ts
@@ -2,3 +2,14 @@ export interface IProfile {
   username: string;
   profile_img_url: string;
 }
+
+export type TendencyType = '새벽' | '아침' | '낮' | '밤';
+
+export interface ITendency {
+  type: TendencyType;
+}
+
+export interface IDashboardData {
+  profile: IProfile;
+  tendency: ITendency;
+}
