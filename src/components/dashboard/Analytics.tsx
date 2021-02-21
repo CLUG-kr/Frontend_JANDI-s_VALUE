0ac60eebe8ae934ca2017a5d790564577bf16188
hoondeveloper <hoonskyn@naver.com>
2021-02-21 22:36:54 +0900

    feat: contribution analytics view

--- src/components/dashboard/Analytics.tsx
+++ src/components/dashboard/Analytics.tsx
@@ -4,17 +4,47 @@ import AnalyticsAsDay from 'components/dashboard/charts/AnalyticsAsDay';
 import AnalyticsAsLanguage from 'components/dashboard/charts/AnalyticsAsLanguage';
 import tw from 'twin.macro';
 import AnalyticsAsTime from 'components/dashboard/charts/AnalyticsAsTime';
+import styled from '@emotion/styled';
+import AnalyticsAsContribution from 'components/dashboard/charts/AnalyticsAsContribution';
 
 interface IAnalyticsProps {
   analytics: IAnalytics;
 }
 
+const AnalyticsItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Analytics: React.FC<IAnalyticsProps> = ({ analytics }) => {
   return (
     <div css={tw`grid grid-cols-2`}>
-      <AnalyticsAsDay data={analytics.day} />
+      <AnalyticsItem>
+        <AnalyticsAsContribution data={analytics.contribution} />
+        <h3 css={tw`text-gray-800 text-lg tracking-tight`}>
+          컨트리뷰션 분석 그래프
+        </h3>
+      </AnalyticsItem>
+      <AnalyticsItem>
+        <AnalyticsAsDay data={analytics.day} />
+        <h3 css={tw`text-gray-800 text-lg tracking-tight`}>
+          요일별 커밋 경향 그래프
+        </h3>
+      </AnalyticsItem>
       <AnalyticsAsLanguage data={analytics.language} />
-      <AnalyticsAsTime data={analytics.time} />
+      <AnalyticsItem>
+        <h3 css={tw`text-gray-800 text-lg tracking-tight`}>
+          언어 사용 경향 그래프
+        </h3>
+      </AnalyticsItem>
+      <AnalyticsItem>
+        <AnalyticsAsTime data={analytics.time} />
+        <h3 css={tw`text-gray-800 text-lg tracking-tight`}>
+          시간별 커밋 경향 그래프
+        </h3>
+      </AnalyticsItem>
     </div>
   );
 };
