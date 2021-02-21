7c50c278867f2fbbdc7934f6e5a4e7197326e239
hoondeveloper <hoonskyn@naver.com>
2021-02-22 01:10:16 +0900

    refactor: temp

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,2 +1,2 @@
 export const SERVER_HOST = 'https://jandi-backend.herokuapp.com';
-export const DEBUG_ACCESS_TOKEN = '11b8951b90b3871604e73ff77c1164da13020a5b';
+export const DEBUG_ACCESS_TOKEN = '2d1cd6cabf8279172c28cc9a83453cca7a409992';
