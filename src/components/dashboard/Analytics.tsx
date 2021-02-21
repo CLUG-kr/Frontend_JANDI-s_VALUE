a59420c96e0030c71abe48777dcfe70c99cdf8c8
hoondeveloper <hoonskyn@naver.com>
2021-02-22 01:45:50 +0900

    refactor: analytics view style

--- src/components/dashboard/Analytics.tsx
+++ src/components/dashboard/Analytics.tsx
@@ -22,28 +22,36 @@ const Analytics: React.FC<IAnalyticsProps> = ({ analytics }) => {
   return (
     <div css={tw`grid grid-cols-1 lg:grid-cols-2 gap-6`}>
       <AnalyticsItem>
-        <AnalyticsAsContribution data={analytics.contribution} />
-        <h3 css={tw`text-gray-800 text-lg tracking-tight mt-4`}>
+        <h3
+          css={tw`w-full text-gray-800 text-xl font-bold tracking-tight mb-4`}
+        >
           컨트리뷰션 분석 그래프
         </h3>
+        <AnalyticsAsContribution data={analytics.contribution} />
       </AnalyticsItem>
       <AnalyticsItem>
-        <AnalyticsAsDay data={analytics.day} />
-        <h3 css={tw`text-gray-800 text-lg tracking-tight mt-4`}>
+        <h3
+          css={tw`w-full text-gray-800 text-xl font-bold tracking-tight mb-4`}
+        >
           요일별 커밋 경향 그래프
         </h3>
+        <AnalyticsAsDay data={analytics.day} />
       </AnalyticsItem>
       <AnalyticsItem>
-        <AnalyticsAsLanguage data={analytics.language} />
-        <h3 css={tw`text-gray-800 text-lg tracking-tight mt-4`}>
+        <h3
+          css={tw`w-full text-gray-800 text-xl font-bold tracking-tight mb-4 mt-8`}
+        >
           언어 사용 경향 그래프
         </h3>
+        <AnalyticsAsLanguage data={analytics.language} />
       </AnalyticsItem>
       <AnalyticsItem>
-        <AnalyticsAsTime data={analytics.time} />
-        <h3 css={tw`text-gray-800 text-lg tracking-tight mt-4`}>
+        <h3
+          css={tw`w-full text-gray-800 text-xl font-bold tracking-tight mb-4 mt-8`}
+        >
           시간별 커밋 경향 그래프
         </h3>
+        <AnalyticsAsTime data={analytics.time} />
       </AnalyticsItem>
     </div>
   );
